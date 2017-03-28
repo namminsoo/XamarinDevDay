142881a848b7b9b3040e9f439991493f657dcfea
Namsoo <Namsoo@Namsooui-MacBook-Pro.local>
2017-03-28 21:10:32 +0900

    add practice project and doc

---
+++ xamarin_101_120/Droid/PhoneDialer.Droid.cs
@@ -0,0 +1,54 @@
+﻿using Android.Content;
+using System.Linq;
+using System.Threading.Tasks;
+using Android.Telephony;
+using Xamarin.Forms;
+using xamarin_101.Droid;
+using static xamarin_101.xamarin_101Page;
+
+using Uri = Android.Net.Uri;
+
+[assembly: Dependency(typeof(PhoneDialer))]
+
+namespace xamarin_101.Droid
+{
+	public class PhoneDialer : IDialer
+	{
+		/// <summary>
+		/// Dial the phone
+		/// </summary>
+		public Task<bool> DialAsync(string number)
+		{
+			var context = Forms.Context;
+			if (context != null)
+			{
+				var intent = new Intent(Intent.ActionCall);
+				intent.SetData(Uri.Parse("tel:" + number));
+
+				if (IsIntentAvailable(context, intent))
+				{
+					context.StartActivity(intent);
+					return Task.FromResult(true);
+				}
+			}
+
+			return Task.FromResult(false);
+		}
+
+		/// <summary>
+		/// Checks if an intent can be handled.
+		/// </summary>
+		public static bool IsIntentAvailable(Context context, Intent intent)
+		{
+			var packageManager = context.PackageManager;
+
+			var list = packageManager.QueryIntentServices(intent, 0)
+				.Union(packageManager.QueryIntentActivities(intent, 0));
+			if (list.Any())
+				return true;
+
+			TelephonyManager mgr = TelephonyManager.FromContext(context);
+			return mgr.PhoneType != PhoneType.None;
+		}
+	}
+}
---
+++ xamarin_101_120/xamarin_101/xamarin_101Page.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace xamarin_101
+{
+	public partial class xamarin_101Page : ContentPage
+	{
+		public xamarin_101Page()
+		{
+			InitializeComponent();
+		}
+
+		private void ButtonClicked(object sender, EventArgs e)
+		{
+			this.DisplayAlert("Alert", "You clicked me", "OK");
+		}
+
+		async void OnCall(object sender, System.EventArgs e)
+		{
+			if (await this.DisplayAlert(
+				"Dial a Number",
+				"Would you like to call " + phoneNumberText.Text + "?",
+				"Yes",
+				"No"))
+			{
+				var dialer = DependencyService.Get<IDialer>();
+				if (dialer != null)
+				{
+					await dialer.DialAsync(phoneNumberText.Text);
+				}
+			}
+		}
+
+		public interface IDialer
+		{
+			Task<bool> DialAsync(string number);
+		}
+
+	}
+}
